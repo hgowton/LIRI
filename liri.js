e2a058803c3d58bb3bcda946f24fa9790bd70f26
hgowton <hgowton@gmail.com>
2020-02-29 14:11:26 -0500

    initial files are set up and Spotify call is being made correctly

---
+++ liri.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const keys = require("./keys.js")
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
