219fb8c4cd2b6d2634b4f805c60ca635d12e7fdd
hgowton <hgowton@gmail.com>
2020-03-03 21:38:02 -0500

    log is working

--- liri.js
+++ liri.js
@@ -3,62 +3,72 @@ const keys = require("./keys.js")
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var fs = require("fs");
+var moment = require('moment');
 
 var a = process.argv[2]
-var b = process.argv[3]
-// console.log(a)
-// console.log(b)
 
-var song = 'The Sign';
-for (let i = 3; i < process.argv.length; i++) {
-  song = song + process.argv[i] + ' ';
-  console.log(song);
-}
-
-var word = 'Mr.Nobody';
-for (let i = 3; i < process.argv.length; i++) {
-  word = word + process.argv[i] + ' ';
-//   console.log(word);
-}
-
-var artist = '';
-for (let i = 3; i < process.argv.length; i++) {
-  artist = artist + process.argv[i];
-//   console.log(artist);
-}
+var term =  process.argv.slice(3).join(" ");
+var divLine = '\n____________'
 
 //Spotify API call
 function songs (){
 
+    //if no term is provided do a call for The Sign
+    if(!term) {
+        term = "The Sign";
+    }
+
     var spotify = new Spotify(keys.spotify);
-    
-    spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
+
+    spotify.search({ type: 'track', query: term, limit: 1 }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
         songI = data.tracks.items
-        console.log(songI); 
-        console.log("Artists: " + songI[0].artists[0].name)
-        console.log("Song Title: " + songI[0].name)
-        console.log("Song Preview: " + songI[0].preview_url)
-        console.log("Album Title: " + songI[0].album.name)        
+        var songInfo = [
+            "\nArtists: " + songI[0].artists[0].name,
+            "Song Title: " + songI[0].name,
+            "Song Preview: " + songI[0].preview_url,
+            "Album Title: " + songI[0].album.name
+        ].join("\n");
+            
+            //append information to the log 
+            fs.appendFile("log.txt", songInfo, function(err) {
+                if (err) throw err;
+                console.log(songInfo);
+            });
+      
     });
 }
 
 //Bands in Town call
 function bands (){
+    if(!term) {
+        term = "paul mccartney";
+    }
     var queryUrl =
-    'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp';
+    'https://rest.bandsintown.com/artists/' + term + '/events?app_id=codingbootcamp';
     console.log(queryUrl);
+
     //makes call for Bands in town with specific artist
     axios.get(queryUrl)
     .then(function(response) {
-        // console.log(response);
+
         for (i = 0; i < response.data.length; i++) {
-            var time = response.data[i].datetime
-            console.log(response.data[i].venue.name)
-            console.log(response.data[i].venue.country + ", " + response.data[i].venue.city)
-            console.log(response.data[i].datetime)
+            var bInfo = response.data[i]
+            var time = bInfo.datetime
+
+        var bandInfo = [
+            "\nVenue: " + bInfo.venue.name,
+            "Location: " + bInfo.venue.country + ", " + bInfo.venue.city,
+            "Date: " + bInfo.datetime].join("\n");
+            
+            //append information to the log 
+            fs.appendFile("log.txt", bandInfo, function(err) {
+                if (err) throw err;
+                console.log(bandInfo);
+            });
+
             console.log(moment(time).format('MMMM Do YYYY h:mm:ss a'))
         }
     })
@@ -85,23 +95,40 @@ function bands (){
 }
 
 //OMBI call
-function movie () {
+function term () {
+
+    //removes first three indexes and joins all remaining index with space
+    var word = process.argv.slice(3).join(" ");
+
+    //if no movie is provided do a call for Mr. Nobody
+    if(!term) {
+        term = "Mr.Nobody";
+    }
+
     var queryUrl =
     'http://www.omdbapi.com/?t=' + word + '&y=&plot=short&apikey=trilogy';
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
-    axios
-    .get(queryUrl)
+    
+    axios.get(queryUrl)
     .then(function(response) {
         console.log(response);
         infoM = response.data
-        console.log(infoM.Title + " was released in " + infoM.Year + ".")
-        console.log("IMDB Rating: " + infoM.imdbRating)
-        console.log("Rotten Tomatoes Rating: " + infoM.Ratings[1].Value)
-        console.log("Produced in " + infoM.Country)
-        console.log("Native Language: " + infoM.Language)
-        console.log("Plot of Movie: " + infoM.Plot)
-        console.log("Actors: " + infoM.Actors)
+
+        var movieInfo = [
+            "Title: " + infoM.Title,
+            "Year:" + infoM.Year,
+            "IMDB Rating: " + infoM.imdbRating,
+            "Rotten Tomatoes Rating: " + infoM.Ratings[1].Value,
+            "Produced in " + infoM.Country,
+            "Native Language: " + infoM.Language,
+            "Plot of Movie: " + infoM.Plot,
+            "Actors: " + infoM.Actors
+        ].join("\n\n");
+    
+        //append information to the log 
+        fs.appendFile("log.txt", movieInfo + divLine, function(err) {
+            if (err) throw err;
+            console.log(movieInfo);
+        });
         
     })
     .catch(function(error) {
@@ -130,8 +157,8 @@ function newS () {
     var data = fs.readFileSync("random.txt", "utf8") 
         var dataArr = data.split(",");
         
-        console.log("needs to happen 1st: " + dataArr[1]);
-        song = dataArr[1]
+        // console.log("needs to happen 1st: " + dataArr[1]);
+        term = dataArr[1]
     }
 
 if (a == "movie-this") {
@@ -144,6 +171,7 @@ if (a == "movie-this") {
     newS();
     songs();
     }
+
 
 
 
